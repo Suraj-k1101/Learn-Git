c5d11af38bac74009300cd0b3f2cf0b45c94bd3a
Suraj-k1101 <surajromankamble@gmail.com>
2023-07-19 11:48:06 +0530

    first commit

---
+++ Repo.java
@@ -0,0 +1,30 @@
+public class Repo {
+    public static void main(String[]args)
+    {
+        String s = "MARKRAM";
+        String rev = "";
+
+        for(int i = s.length()-1;i>=0;i--)
+        {
+
+           rev = rev + s.charAt(i);
+
+        }
+        
+        if(s.equals(rev))
+        {
+
+           System.out.println("String is palindrome");
+
+        }
+        else
+        {
+
+            System.out.println("String is not palindrome"); 
+
+        }
+
+
+
+    }
+}
